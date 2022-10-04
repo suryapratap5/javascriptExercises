9854e5790985a86cecefce94c9c895b388fd7f23
surya pratap <suryapratapsoni5@gmail.com>
2022-10-04 11:31:19 +0530

    Program for removing duplicate items from an array

---
+++ Array/Delete_duplicate_element.js
@@ -0,0 +1,29 @@
+let arr = [2, 5, 8, 3, 8 ,2 , 7, 34, 7, 5, 0, 7];
+let new_array = [];
+
+// method - 1
+// for(let i of arr){
+//     if(new_array.indexOf(i) === -1){
+//         new_array.push(i);
+        
+//     }
+// }
+// console.log(new_array)
+
+// method - 2
+
+// program to remove duplicate value from an array
+
+// function getUnique(arr){
+
+    // removing duplicate
+    // let uniqueArr = [...new Set(arr)];
+    // console.log(uniqueArr);
+//     console.log(...new Set(arr))
+// }
+
+// const array = [1, 2, 3, 2, 3];
+// calling the function
+// getUnique(array);
+
+
